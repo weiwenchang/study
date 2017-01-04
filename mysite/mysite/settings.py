b3bc962990c03a434078e40a67e39f4252b5aea9
we <986431417@qq.com>
2017-01-04 15:54:45 +0800

    add base

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -109,6 +109,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES=[
+STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static'),
 ]
--- mysite/user_info/models.py
+++ mysite/user_info/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class user_info(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=16)
+    ugender = models.BooleanField()
+    utell = models.IntegerField(max_length=11)
+    uaddress = models.CharField(max_length=40)
+    isDelete = models.BooleanField()
+    def __unicode__(self):
+        return self.pk
