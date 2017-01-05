d1a9cccdb80dd0f1bd086de704c1cdf9d51dc082
zhou <752561752@qq.com>
2017-01-05 16:20:51 +0800

    two commit

--- mysite/user_center/models.py
+++ mysite/user_center/models.py
@@ -1,3 +1,14 @@
+#coding=utf8
 from django.db import models
+from user_info.models import *
 
 # Create your models here.
+
+
+class userCenter(models.Model):
+    uc_name = models.CharField(max_length=10)
+    uc_address = models.CharField(max_length=100)
+    uc_phone = models.CharField(max_length=15)
+    uc_zipcode = models.CharField(max_length=10)
+    uc_isDelete = models.BooleanField(default=False)
+    uc_user = models.ForeignKey('user_info.user_info1')
